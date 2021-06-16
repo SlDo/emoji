acbc1f8654472610f2b1f45a80d39d33c5d20f34
Слава <slavadodonov19@gmail.com>
2021-06-16 13:32:40 +0300

    refactoring

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,109 +1,121 @@
 const scrollbar = document.querySelector("[data-scroll='container']");
-const scrollLine = scrollbar.querySelector(".scrollbar__line");
+const scrollLine = scrollbar.querySelector('.scrollbar__line');
 const scrollContent = document.querySelector("[data-scroll='content']");
 
-const getElementHeight = (elem) =>
-  "getComputedStyle" in window
-    ? parseInt(
-        window.getComputedStyle(elem, null).getPropertyValue("height"),
-        10
-      )
-    : parseInt(elem.currentStyle.height, 10);
+const getElementHeight = (elem) => ('getComputedStyle' in window
+  ? parseInt(
+    window.getComputedStyle(elem, null).getPropertyValue('height'),
+    10,
+  )
+  : parseInt(elem.currentStyle.height, 10));
 
-const scrollbarHeight = getElementHeight(scrollbar);
-const contentScrollHeight = scrollContent.scrollHeight;
-const ratio = getElementHeight(scrollContent) / contentScrollHeight;
+const getTranslateY = (element) => {
+  if (!('getComputedStyle' in window)) return;
 
-let startTransform = 0;
+  const computedStyle = getComputedStyle(element);
+  const crossBrowserTransform = computedStyle.transform
+    || computedStyle.webkitTransform
+    || computedStyle.mozTransform;
+  let parseMatrix = crossBrowserTransform.match(/^matrix3d\((.+)\)$/);
 
-scrollLine.style.height = `${scrollbarHeight * ratio}px`;
+  if (parseMatrix) return parseFloat(parseMatrix[1].split(', ')[12]);
 
-let startY = 0;
-const limitY = scrollbarHeight - getElementHeight(scrollLine);
+  parseMatrix = crossBrowserTransform.match(/^matrix\((.+)\)$/);
 
-const transformScroll = (e) => {
-  const transform = startTransform + e.clientY - startY;
-
-  if (transform >= 0 && transform <= limitY) {
-    scrollLine.style.transform = `translateY(${transform}px)`;
-    scrollContent.style.transform = `translateY(${-transform / ratio}px)`;
-  }
+  return parseMatrix ? parseFloat(parseMatrix[1].split(', ')[5]) : 0;
 };
 
-const scroll = (e) => {
-  startTransform = +/\.*translateY\((.*)px\)/i.exec(
-    scrollLine.style.transform
-  )[1];
-  scrollLine.style.transition = "0s";
-  scrollContent.style.transition = "0s";
-  startY = e.clientY;
+const addCustomScroll = (scrollbarElement, scrollLineElement, scrollContentElement) => {
+  const scrollbarHeight = getElementHeight(scrollbarElement);
+  const scrollContentHeight = getElementHeight(scrollContentElement);
 
-  document.addEventListener("mousemove", transformScroll);
+  // Высота с областью прокрутки
+  const contentScrollHeight = scrollContentElement.scrollHeight;
+
+  const ratio = scrollContentHeight / contentScrollHeight;
+  const scrollLineHeight = scrollbarHeight * ratio;
+
+  let startTransform = 0;
+  let startY = 0;
+  const limitY = scrollbarHeight - scrollLineHeight;
+
+  scrollLineElement.style.height = `${scrollLineHeight}px`;
+
+  const scrollTransform = (scrollLineTransform, scrollContentTransform) => {
+    scrollLineElement.style.transform = `translateY(${scrollLineTransform}px)`;
+    scrollContentElement.style.transform = `translateY(${scrollContentTransform}px)`;
+  };
+
+  const mouseMoveHandle = (e) => {
+    const transform = startTransform + e.clientY - startY;
+
+    if (transform >= 0 && transform <= limitY) scrollTransform(transform, -transform / ratio);
+  };
+
+  const mouseDownHandle = (e) => {
+    startTransform = getTranslateY(scrollLineElement);
+
+    // Сбросить transition-duration на время скроллинга;
+    scrollLineElement.style.transition = '0s';
+    scrollContentElement.style.transition = '0s';
+
+    startY = e.clientY;
+
+    document.addEventListener('mousemove', mouseMoveHandle);
+  };
+
+  scrollLineElement.addEventListener('mousedown', mouseDownHandle);
+  document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', mouseMoveHandle);
+
+    // Возвращает transition-duration
+    scrollLineElement.style.transition = null;
+    scrollContentElement.style.transition = null;
+  });
+  // TODO: Сделать прокрутку медленнее
+  scrollbarElement.addEventListener('wheel', (e) => {
+    const currentTransform = getTranslateY(scrollLineElement);
+
+    if (e.deltaY > 0) {
+      const delta = currentTransform + scrollLineElement.scrollHeight / 20 / ratio;
+
+      if (delta < limitY) {
+        scrollTransform(delta, -(delta / ratio));
+      } else {
+        scrollTransform(limitY, (scrollContentElement.clientHeight - contentScrollHeight));
+      }
+    } else if (e.deltaY < 0) {
+      const delta = currentTransform - scrollLineElement.clientHeight / 20 / ratio;
+
+      if (delta > 0) {
+        scrollTransform(delta, -(delta / ratio));
+      } else {
+        scrollTransform(0, 0);
+      }
+    }
+  });
+
+  scrollContentElement.addEventListener('wheel', (e) => {
+    const currentTransform = getTranslateY(scrollContentElement);
+
+    if (e.deltaY > 0) {
+      const delta = currentTransform - (contentScrollHeight / 20);
+
+      if (-delta * ratio <= limitY) {
+        scrollTransform(-delta * ratio, delta);
+      } else {
+        scrollTransform(limitY, scrollContentElement.clientHeight - contentScrollHeight);
+      }
+    } else if (e.deltaY < 0) {
+      const delta = currentTransform + (contentScrollHeight / 20);
+
+      if (-delta * ratio >= 0) {
+        scrollTransform(-delta * ratio, delta);
+      } else {
+        scrollTransform(0, 0);
+      }
+    }
+  });
 };
 
-scrollLine.addEventListener("mousedown", scroll);
-document.addEventListener("mouseup", () => {
-  document.removeEventListener("mousemove", transformScroll);
-  scrollLine.style.transition = null;
-  scrollContent.style.transition = null;
-});
-
-scrollbar.addEventListener("wheel", (e) => {
-  const currentTransform = +/\.*translateY\((.*)px\)/i.exec(
-    scrollLine.style.transform
-  )[1];
-
-  if (e.deltaY > 0) {
-    const delta = currentTransform + scrollLine.scrollHeight / 20 / ratio;
-
-    if (delta < limitY) {
-      scrollLine.style.transform = `translateY(${delta}px)`;
-      scrollContent.style.transform = `translateY(-${delta / ratio}px)`;
-    } else {
-      scrollContent.style.transform = `translateY(${
-        scrollContent.clientHeight - contentScrollHeight
-      }px)`;
-      scrollLine.style.transform = `translateY(${limitY}px)`;
-    }
-  } else if (e.deltaY < 0) {
-    const delta = currentTransform - scrollLine.clientHeight / 20 / ratio;
-
-    if (delta > 0) {
-      scrollLine.style.transform = `translateY(${delta}px)`;
-      scrollContent.style.transform = `translateY(-${delta / ratio}px)`;
-    } else {
-      scrollContent.style.transform = `translateY(${0}px)`;
-      scrollLine.style.transform = `translateY(${0}px)`;
-    }
-  }
-});
-
-scrollContent.addEventListener("wheel", (e) => {
-  const currentTransform = +/\.*translateY\((.*)px\)/i.exec(
-    scrollContent.style.transform
-  )[1];
-
-  if (e.deltaY > 0) {
-    const delta = currentTransform - (scrollContent.scrollHeight / 20) * 0.1;
-
-    if (-delta * ratio <= limitY) {
-      scrollLine.style.transform = `translateY(${-delta * ratio}px)`;
-      scrollContent.style.transform = `translateY(${delta}px)`;
-    } else {
-      scrollContent.style.transform = `translateY(${
-        scrollContent.clientHeight - contentScrollHeight
-      }px)`;
-      scrollLine.style.transform = `translateY(${limitY}px)`;
-    }
-  } else if (e.deltaY < 0) {
-    const delta = currentTransform + (scrollContent.scrollHeight / 20) * 0.1;
-
-    if (-delta * ratio >= 0) {
-      scrollLine.style.transform = `translateY(${-delta * ratio}px)`;
-      scrollContent.style.transform = `translateY(${delta}px)`;
-    } else {
-      scrollContent.style.transform = `translateY(${0}px)`;
-      scrollLine.style.transform = `translateY(${0}px)`;
-    }
-  }
-});
+addCustomScroll(scrollbar, scrollLine, scrollContent);
--- src/scripts/tabs.js
+++ src/scripts/tabs.js
@@ -1,25 +1,54 @@
 const tabs = document.querySelectorAll("[data-emoji-section='tab']");
-const tabsIcon = document.querySelectorAll("[data-action='tabIcon']");
 const allEmojiButton = document.querySelector('[data-tab="all"]');
 const recentlyEmojiButton = document.querySelector('[data-tab="recently"]');
 
-const setActiveIcon = (tabIndex) => {
-  tabsIcon.forEach((tab) => tab.classList.remove("active"));
-  tabsIcon[tabIndex].classList.add("active");
+const hasProperty = (object, property) => Object.prototype.hasOwnProperty.bind(object, property);
+
+const createTabs = (settings) => {
+  const setTab = (tab) => {
+    Object.keys(settings).forEach((key) => {
+      const tabSetting = settings[key];
+      const hasActiveButtonClass = hasProperty(tabSetting, 'activeButtonClass');
+
+      if (hasProperty(tabSetting, 'tabContent')) {
+        if (tab === key) {
+          if (hasActiveButtonClass) tabSetting.button.classList.add('active');
+          tabSetting.tabContent.classList.remove('inactive-tab');
+        } else {
+          if (hasActiveButtonClass) tabSetting.button.classList.remove('active');
+          tabSetting.tabContent.classList.add('inactive-tab');
+        }
+      }
+    });
+  };
+
+  Object.keys(settings).forEach((key) => {
+    const tabSetting = settings[key];
+
+    if (hasProperty(tabSetting, 'tabContent')) {
+      tabSetting.tabContent.classList.add('inactive-tab');
+    }
+
+    if (hasProperty(tabSetting, 'button')) {
+      tabSetting.button.addEventListener('click', () => setTab(key));
+    }
+
+    if (hasProperty(tabSetting, 'default') && tabSetting.default === true) {
+      setTab(key);
+    }
+  });
 };
 
-const setTab = (tabIndex) => {
-  tabs.forEach((tab) => tab.classList.add("hidden"));
-  tabs[tabIndex].classList.remove("hidden");
-  setActiveIcon(tabIndex);
-};
-
-setTab(0);
-
-allEmojiButton.addEventListener("click", () => {
-  setTab(0);
-});
-
-recentlyEmojiButton.addEventListener("click", () => {
-  setTab(1);
+createTabs({
+  1: {
+    tabContent: tabs[0],
+    button: allEmojiButton,
+    activeButtonClass: 'active',
+    default: true,
+  },
+  2: {
+    tabContent: tabs[1],
+    activeButtonClass: 'active',
+    button: recentlyEmojiButton,
+  },
 });
